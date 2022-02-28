d3365b6d2e7b25266ea0b82cbecdacb354497b70
Grotok <79658883+Grotok@users.noreply.github.com>
2022-02-27 21:46:03 -0700

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+function hideAll() {
+    var x = document.getElementById("hideall");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function hideAlso() {
+    var x = document.getElementById("hidealso");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
